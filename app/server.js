c108fc3c44ee09a384c769b3bb5451c9bdd1849f
Isac Lehmkuhl dos Santos <isac.santos@digitro.com>
2026-03-12 09:03:44 -0300

    feat: add nginx on compose and config it

--- app/server.js
+++ app/server.js
@@ -1,8 +1,8 @@
-import "dotenv/config"
+import "dotenv/config";
 import app from "./src/app.js";
 
-const PORT = 3000
+const PORT = process.env.APP_PORT;
 
 app.listen(PORT, () => {
-    console.log("Escutando")
+  console.log("Escutando");
 });
--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -2,9 +2,15 @@ import express from "express";
 import posts from "./postsRoutes.js";
 
 const routes = (app) => {
-  app.route("/").get((req, res) => res.status(200).send("Curso de node.js"));
-
-  app.use(express.json(), posts);
+  (app.route("/").get((req, res) => {
+    // healthcheck
+    res.status(200).json({
+      status: "OK",
+      uptime: process.uptime(),
+      timestamp: Date.now(),
+    });
+  }),
+    app.use(express.json(), posts));
 };
 
 export default routes;
